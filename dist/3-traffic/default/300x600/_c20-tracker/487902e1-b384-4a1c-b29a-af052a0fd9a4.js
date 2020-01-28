cf44de39a9e85486b8ca7e273a3d6a824be01d13
Derek Miranda <derek.miranda@ff0000.com>
2020-01-28 13:40:21 -0800

    Vert align fix - Thai

--- dist/3-traffic/default/300x600/_c20-tracker/487902e1-b384-4a1c-b29a-af052a0fd9a4.js
+++ dist/3-traffic/default/300x600/_c20-tracker/487902e1-b384-4a1c-b29a-af052a0fd9a4.js
@@ -19,7 +19,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1580240709546,
+  "updated": 1580247606263,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-300x600",
   "html_url": "https://github.com/NetflixDev/bt-ER-300x600",
