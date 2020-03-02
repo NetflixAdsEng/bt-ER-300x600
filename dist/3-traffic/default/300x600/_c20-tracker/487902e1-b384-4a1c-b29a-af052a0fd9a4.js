69219f59a98d5fd248456593ea90b45984473e48
Derek Miranda <hello@derekmiranda.com>
2020-03-02 13:14:09 -0800

    Removes horizontal padding for 300x600 corner layouts

--- dist/3-traffic/default/300x600/_c20-tracker/487902e1-b384-4a1c-b29a-af052a0fd9a4.js
+++ dist/3-traffic/default/300x600/_c20-tracker/487902e1-b384-4a1c-b29a-af052a0fd9a4.js
@@ -20,7 +20,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1582931969088,
+  "updated": 1583180886650,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-300x600",
   "html_url": "https://github.com/NetflixDev/bt-ER-300x600",
