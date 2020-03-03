73b8b18a744070f25e11d6af8b25e5570a16915a
Derek Miranda <hello@derekmiranda.com>
2020-03-03 10:41:07 -0800

    Removes unpkg domain

--- dist/1-build/300x600/_c20-tracker/487902e1-b384-4a1c-b29a-af052a0fd9a4.js
+++ dist/1-build/300x600/_c20-tracker/487902e1-b384-4a1c-b29a-af052a0fd9a4.js
@@ -20,7 +20,7 @@ module.exports = {
   },
   "context": "RED Interactive",
   "state": "published",
-  "updated": 1583203406456,
+  "updated": 1583260863695,
   "username": "GitHub",
   "full_name": "NetflixDev/bt-ER-300x600",
   "html_url": "https://github.com/NetflixDev/bt-ER-300x600",
